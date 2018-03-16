d9bc8ceb57546342359c56ad150cf214292a0d43
HWS <15757135983@163.com>
2018-03-16 12:06:01 +0800

    1.初始化react项目

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';/**
+ * Created by HHH on 2018/3/16.
+ */
+
+const element = <h1>Hello, world!</h1>;
+ReactDOM.render(
+  element,
+  document.getElementById("root")
+);
